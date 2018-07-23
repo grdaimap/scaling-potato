eb389675951b9b44c443f40f7d3a7501a624bbbe
grdaimap <36882078+grdaimap@users.noreply.github.com>
2018-07-23 18:45:07 +0800

    更新py，因为pip 10.0以上修改了方法

--- python_update/update.py
+++ python_update/update.py
@@ -1,4 +1,17 @@
+﻿# -*- coding:utf-8 -*-
+'''
+@Author  : Soner
+@version :
+@Time    : 2017/11/3/0003 15:11
+@license : Copyright(C), Your Company
+'''
 import pip
+# pip V10.0.0以上版本需要导入下面的包
+from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
-for package in pip.get_installed_distributions():
-   call('pip install --upgrade ' + package.project_name + " -i https://pypi.tuna.tsinghua.edu.cn/simple")
+from time import sleep
+
+for dist in get_installed_distributions():
+    # 执行后，pip默认为Python3版本
+    # 双版本下需要更新Python2版本的包，使用py2运行，并将pip修改成pip2
+    call("pip install --upgrade " + dist.project_name, shell=True)
