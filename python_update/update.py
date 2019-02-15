ac14dabc98803ae910ad4349136140ec47ee63f5
grdaimap <36882078+grdaimap@users.noreply.github.com>
2019-02-15 13:03:15 +0800

    更新

--- python_update/update.py
+++ python_update/update.py
@@ -1,5 +1,6 @@
 ﻿# -*- coding:utf-8 -*-
 '''
+尊重原作者，在他基础上加入pip升级和选择国内源
 @Author  : Soner
 @version :
 @Time    : 2017/11/3/0003 15:11
@@ -11,7 +12,9 @@ from pip._internal.utils.misc import get_installed_distributions
 from subprocess import call
 from time import sleep
 
+call("python -m pip install --upgrade pip ", shell=True)
+
 for dist in get_installed_distributions():
     # 执行后，pip默认为Python3版本
     # 双版本下需要更新Python2版本的包，使用py2运行，并将pip修改成pip2
-    call("pip install --upgrade " + dist.project_name, shell=True)
+    call("pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ --upgrade " + dist.project_name, shell=True)
